e014bd352739a0608dce7c168ca0288fc1053be5
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-07-07 17:42:31 +0200

    fix flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -10,7 +10,9 @@ from cinema.views import (
     CinemaHallViewSet,
 )
 
-cinema_hall_list = CinemaHallViewSet.as_view(actions={"get": "list", "post": "create",})
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={"get": "list", "post": "create"}
+)
 cinema_hall_detail = CinemaHallViewSet.as_view(
     actions={
         "get": "retrieve",
